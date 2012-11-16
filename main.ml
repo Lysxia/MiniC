f11c495e641768561eca94273a4d0331121c8bdc
Syrak <li-yao.xia@yahoo.fr>
2012-11-16 01:29:15 +0100

    forgot that one

--- main.ml
+++ main.ml
@@ -1,5 +1,6 @@
 let parse_only = ref false
 let type_only = ref false
+let batch = ref false
 
 let usage = Printf.sprintf
   "Usage : %s program.c [-parse-only] [-type-only]"
@@ -9,30 +10,42 @@ let speclist = [
   "-parse-only",Arg.Unit (fun () -> parse_only := true),
     "Exit after parsing";
   "-type-only",Arg.Unit (fun () -> type_only := true),
-    "Exit after typing"
+    "Exit after typing";
+  "-batch",Arg.Unit (fun () -> batch := true),
+    "Compile multiple files (separately)"
   ]
 
 let args = ref []
 
 let collect arg = args := arg :: !args
 
+let compile file =
+  let h = open_in file in
+  let lexbuf = Lexing.from_channel h in
+  try
+    let ast = Parser.prog Lexer.token lexbuf in
+    if !parse_only then exit 0;
+    let tast = Typing.type_prog ast in
+    0;
+  with
+    | Error.E (sp,ep,s) -> Error.prerr file sp ep s; 1
+    | Parser.Error -> Error.catch file lexbuf; 1
+    | _ -> Printf.eprintf "Unexpected error.\n%!"; 2
+
+
 let () =
-  Arg.parse speclist collect usage;
-  match !args with
-    | [] ->
-      Printf.eprintf "No file to compile specified.\n%s\n" usage;
-      exit 2;
-    | [file] -> (try
-        let h = open_in file in
-        let lexbuf = Lexing.from_channel h in
-        let ast = Parser.prog Lexer.token lexbuf in
-        if !parse_only then exit 0;
-        let tast = Typing.type_prog ast in
-        exit 0;
-      with
-        | Error.E (sp,ep,s) -> Error.prerr sp ep s; exit 1;
-        | _ -> Printf.eprintf "Unexpected error.\n";
-          exit 2)
-    | _ ->
-      Printf.eprintf "Too many arguments.\n%s\n" usage;
-      exit 2;
+  try 
+    Arg.parse speclist collect usage;
+    let rec main = function
+      | [] ->
+        Printf.eprintf "No file to compile specified.\n%s\n%!" usage;
+        exit 2;
+      | [file] -> exit (compile file)
+      | file::t -> if !batch
+        then begin ignore (compile file); main t end
+        else begin
+          Printf.eprintf "Too many arguments.\n%s\n%!" usage;
+          exit 2; end
+    in main !args
+  with
+    | _ -> Printf.eprintf "Unexpected error.\n%!"; exit 2
